dd96c9c83231ef5a2c18bab1149b46a24c6854d8
khanm2758 <khanm2758@gmail.com>
2025-04-03 22:48:55 +0600

    included in this project

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////
+
+import { defineConfig } from "vite";
+import lightningcss from "vite-plugin-lightningcss";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  base: "/safeNetDefender/", // Add this line
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // ✅ Define alias for "src"
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        // @use "@/styles/abstracts/_variables.scss" as *;
+        // @use "@/styles/abstracts/_mixins.scss" as *;
+        // @use "@/styles/abstracts/_functions.scss" as *;
+        
+        `, // ✅ Use "@use" with "as *"
+      },
+    },
+  },
+
+  plugins: [
+    lightningcss({
+      browserslist: "last 2 versions",
+      minify: true,
+    }),
+  ],
+});
